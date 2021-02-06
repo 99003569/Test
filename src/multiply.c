7432e34a45ce9a752801d1ebeebe0bc2de437f61
99003569 <78537068+99003569@users.noreply.github.com>
2021-02-06 14:16:59 +0530

    Add files via upload

---
+++ src/multiply.c
@@ -0,0 +1,8 @@
+#include "header.h"
+
+int multiply(int num1,int num2)
+{
+	int multiplication=0;
+	multiplication = num1*num2;
+	return multiplication;
+}
---
+++ src/subtract.c
@@ -0,0 +1,8 @@
+#include "header.h"
+
+ int subtract(int num1,int num2)
+{
+	int sub=0;
+	sub = num1-num2;
+	return sub;
+}
